f973541ab2e20b5bf01306cd334795251f03eb3b
matty-04 <129518838+matty-04@users.noreply.github.com>
2024-08-14 11:10:16 -0700

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-let menumodal = document.querySelector(".menu-bar");
-
-function menubar(){
-  menumodal.classList.add("active-menu");
-}
-
-function menuclose(){
-  menumodal.classList.remove("active-menu");
-}
