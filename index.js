1e810ef84f32b0f0bd5119b1859be4e0ea70fcf6
matty-04 <129518838+matty-04@users.noreply.github.com>
2024-08-16 18:05:55 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+let menumodal = document.querySelector(".menu-bar");
+
+function menubar(){
+  menumodal.classList.add("active-menu");
+}
+
+function menuclose(){
+  menumodal.classList.remove("active-menu");
+}
+
+function biographyclose(){
+  menumodal.classList.remove("active-menu");
+}
+
+function interestclose(){
+  menumodal.classList.remove("active-menu");
+}
+
+function achievementclose(){
+  menumodal.classList.remove("active-menu");
+}
